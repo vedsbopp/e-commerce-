e1fc294c53a3249876ffe5bd72d4eba80117a5ea
vedsbopp <vedantmistry8901@gmail.com>
2025-08-02 14:17:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const products = [
   { name: "Tiger", price: 390000, image: "kartik-iyer-DJ4vjcD0s0I-unsplash.jpg" },
   { name: "Lion", price: 510000, image: "jean-wimmerlin-FC4GY9nQuu0-unsplash.jpg" },
   { name: "Dogesh Bhai", price: 999999, image: "20240325_131006.jpg" },
-  { name: "Dogesh Bhai", price: 999999, image: "20240325_131006.jpg" }
+  { name: "Gorilla", price: free ("optional"), image: "20240325_131006.jpg" }
 ];
 
 let cart = [];
@@ -68,5 +68,6 @@ function buyNow() {
 
 // Start the app
 displayProducts();
+
 
 
